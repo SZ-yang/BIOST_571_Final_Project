2e88e3403c52781c8cc8d32816cca00343b6a880
amywatt <amywatt97@gmail.com>
2025-03-16 12:22:12 -0700

    added analysis for more methods and summarized results

--- edgeR_limma.R
+++ edgeR_limma.R
@@ -1,13 +1,21 @@
 library(edgeR)
+library(DESeq2)
+library(lmerSeq)
+library(tidyr)
 
 load("/Users/amywatt1/Documents/UW/BIOST_571_Final_Project/simulated_rnaseq_data.RData")
 rnaseq_data_family$counts[1:5, 1:5]
 head(rnaseq_data_family$design)
 
 
+X <- as.data.frame(rnaseq_data_subject$design)
+rownames(X) <- rep(1:nrow(X))
 
-Y <- as.data.frame(rnaseq_data_family$counts)
-X <- as.data.frame(rnaseq_data_family$design)
+Y <- as.data.frame(rnaseq_data_subject$counts)
+rownames(Y) <- rownames(X)
+
+head(X)
+head(t(Y)[1:5, 1:5])
 
 group <- factor(X$condition)
 design <- model.matrix(~group)
@@ -19,17 +27,22 @@ design <- model.matrix(~group)
 # Create DGEList object
 d0 <- DGEList(counts = t(Y)) # func takes a df with rows as genes and columns as samples
 
+# Filter genes
+keep <- filterByExpr(d0, design)
+d0 <- d0[keep, , keep.lib.sizes=FALSE]
+
+# Normalization
+d0 <- normLibSizes(d0)
+
 # Estimate dispersion parameter
 d1 <- estimateDisp(d0, design, robust=TRUE) # estimate common dispersion and tagwise dispersion in one run
 
 # Differential Expression
 fit <- glmFit(d1)
 lrt <- glmLRT(fit)
-edgeR_res <- (lrt$table[1])[,1]
+edgeR_res <- topTags(lrt, adjust.method = "BH", sort.by = "none", n = Inf)
 
-head(lrt$table)
 head(edgeR_res)
-
 
 #####################
 ## limma voom 
@@ -38,14 +51,94 @@ head(edgeR_res)
 # Create DGEList object
 d0 <- DGEList(counts = t(Y))
 
+# Filter genes
+keep <- filterByExpr(d0, design)
+d0 <- d0[keep, , keep.lib.sizes=FALSE]
+
+# Normalization
+d0 <- normLibSizes(d0)
+
 # Voom transformation and calculation of variance weights
 v <- voom(d0, design)
 
 # Fitting linear models in limma
 fit <- lmFit(v, design)
 fit <- eBayes(fit) # Empirical Bayes smoothing of standard errors
-limma_res <- unname(fit$coefficients[,2])
+limma_res <- topTable(fit, adjust.method = "BH", sort.by = "none", n = Inf)
 
-head(fit$coefficients)
-head(fit$p.value)
 head(limma_res)
+
+#####################
+## DESeq
+#####################
+
+# Create DGEList object
+d0 <- DGEList(counts = t(Y)) # func takes a df with rows as genes and columns as samples
+
+# Filter genes
+keep <- filterByExpr(d0, design)
+d0 <- d0[keep, , keep.lib.sizes=FALSE]
+
+# Analysis is performed on simulated data transformed using the vst func in DESeq2
+Y_adjusted <- d0$counts + 1  # Add a pseudo-count to avoid zero values
+dds <- DESeqDataSetFromMatrix(countData = Y_adjusted,
+                              colData = X,
+                              design = ~ condition)
+
+# run DESeq
+dds <- DESeq(dds)
+
+# get results
+de_res <- results(dds, tidy=TRUE)
+head(de_res)
+
+#####################
+## lmerSeq 
+#####################
+
+# Create DGEList object
+d0 <- DGEList(counts = t(Y)) # func takes a df with rows as genes and columns as samples
+
+# Filter genes
+keep <- filterByExpr(d0, design)
+d0 <- d0[keep, , keep.lib.sizes=FALSE]
+
+# Analysis is performed on simulated data transformed using the vst func in DESeq2
+Y_adjusted <- d0$counts + 1  # Add a pseudo-count to avoid zero values
+dds <- DESeqDataSetFromMatrix(countData = Y_adjusted,
+                              colData = X,
+                              design = ~ condition)
+dds <- DESeq(dds)
+vsd.fixed <- varianceStabilizingTransformation(dds, blind=F)
+vst_expr <- assay(vsd.fixed)
+
+
+# Fit the Model
+fit.lmerSeq <- lmerSeq.fit(form = ~ condition + (1|subject_id),
+                           expr_mat = vst_expr,
+                           sample_data = X,
+                           REML = T)
+
+##  Summarize the condition coefficients
+lmer_res <- lmerSeq.summary(lmerSeq_results = fit.lmerSeq,
+                                   coefficient = "conditioncontrol",
+                                   p_adj_method = 'BH',
+                                   ddf = 'Satterthwaite',
+                                   sort_results = F, 
+                                   include_singular = TRUE)
+
+head(lmer_res$summary_table)
+
+
+#########################
+## Put the results together
+#########################
+
+results <- data.frame(
+  edgeR = edgeR_res$table$FDR, 
+  limma = limma_res$adj.P.Val,
+  deseq = de_res$padj,
+  lmerSeq = lmer_res$summary_table$p_val_adj
+)
+
+rownames(results) <- rownames(edgeR_res$table)
